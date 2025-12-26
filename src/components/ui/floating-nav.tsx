53e5b8d0ae14f28666c800235eb6e1130f29b71c
Aiman Ahmad <aimanahmad0004@gmail.com>
2025-12-26 10:33:06 +0000

    why login? change to contact, and can you make it not in the top

--- src/components/ui/floating-nav.tsx
+++ src/components/ui/floating-nav.tsx
@@ -55,17 +55,17 @@ export const FloatingNav = ({
       <motion.div
         initial={{
           opacity: 1,
-          y: -100,
+          y: 100,
         }}
         animate={{
-          y: visible ? 0 : -100,
+          y: visible ? 0 : 100,
           opacity: visible ? 1 : 0,
         }}
         transition={{
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2 items-center justify-center space-x-4 md:hidden",
+          "flex max-w-fit fixed bottom-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2 items-center justify-center space-x-4 md:hidden",
           className
         )}
       >
@@ -82,10 +82,14 @@ export const FloatingNav = ({
             <span className="hidden sm:block text-sm">{navItem.name}</span>
           </Link>
         ))}
-         <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-[10px] py-[6px] rounded-full">
-          <span>Login</span>
+         <a 
+          href="#contact"
+          onClick={(e) => handleClick(e, "#contact")}
+          className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-[10px] py-[6px] rounded-full"
+        >
+          <span>Contact</span>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </button>
+        </a>
       </motion.div>
     </AnimatePresence>
   );
