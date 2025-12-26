7173eb8c01115691a6b72ce5bd7e978d0b3946b2
Aiman Ahmad <aimanahmad0004@gmail.com>
2025-12-26 09:49:19 +0000

    add color to the colourfull text

--- src/components/ui/text-generate-effect.tsx
+++ src/components/ui/text-generate-effect.tsx
@@ -32,7 +32,7 @@ export const TextGenerateEffect = ({
           return (
             <motion.span
               key={word + idx}
-              className="dark:text-white text-black opacity-0"
+              className="text-foreground opacity-0"
             >
               {word}{" "}
             </motion.span>
@@ -45,7 +45,7 @@ export const TextGenerateEffect = ({
   return (
     <div className={cn("font-normal", className)}>
       <div className="mt-4">
-        <div className=" dark:text-white text-black leading-snug tracking-wide">
+        <div className="text-foreground leading-snug tracking-wide">
           {renderWords()}
         </div>
       </div>
