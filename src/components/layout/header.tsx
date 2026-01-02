5292defd8ccd41cddaa6c8113f4a6996ff33968e
Mohamad Aiman Bin Ahmad <Aiman.Ahmad@prasarana.com.my>
2026-01-02 13:12:16 +0800

    ädjust webmanifest

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -51,7 +51,7 @@ export default function Header() {
       icon: <Briefcase className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
-      name: "Package",
+      name: "Collection",
       link: "#portfolio",
       icon: <Briefcase className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
